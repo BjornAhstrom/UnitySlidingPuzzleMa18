a5bfc6003ad8e16aaff4caad19f099906924b6fe
Björn Åhström <ahstrom_81@hotmail.com>
2019-08-22 14:32:10 +0200

    first commit

---
+++ Assets/_Game/Scripts/TileController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileController : MonoBehaviour
+{
+    public TileManager tileManager;
+    //public bool empty = false;
+
+    private void OnMouseDown()
+    {
+        //Debug.Log("Touch " + gameObject.name);
+
+         tileManager.TilePressed(gameObject, false);
+    }
+}
